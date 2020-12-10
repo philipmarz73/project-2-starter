dc9e0eec83d8e594eff07171ddbdaf49ee5dad0a
Phlipper <marzullop73@gmail.com>
2020-12-09 23:12:36 -0500

    added playerGames.js file and defined datatypes

--- models/player.js
+++ models/player.js
@@ -1,12 +1,1 @@
-module.exports = function (sequelize, DataTypes) {
-    const Player = sequelize.define("Player", {
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        phone: DataTypes.STRING,
-        //TODO: Days of the week? 
-    })
-
-    return Player;
-};
+added code to define 'player a'
---
+++ models/playerGames.js
@@ -0,0 +1,13 @@
+module.exports = function (sequelize, DataTypes) {
+    const PlayerGames = sequelize.define(
+        "PlayerGames",
+        {
+            playerId: DataTypes.INTEGER,
+            gameId: DataTypes.INTEGER,
+        },
+    {
+        timestamps: false,
+    }
+    );
+    return PlayerGames;
+};
